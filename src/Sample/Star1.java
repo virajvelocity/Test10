b2ed46c2a7ea6d1999d2b3425f7e11567e7fc84d
VIRAJ <VIRAJ@DESKTOP-U6OK72A>
2023-01-16 16:40:31 +0530

    Hi

---
+++ src/Sample/Star1.java
@@ -0,0 +1,19 @@
+package Sample;
+
+public class Star1 
+{
+	
+	//****
+		public static void main(String[] args)
+		{
+			
+			for(int i=1;i<=4;i++)
+			{
+				System.out.print("*");
+			}
+			
+		}
+
+	
+
+}
